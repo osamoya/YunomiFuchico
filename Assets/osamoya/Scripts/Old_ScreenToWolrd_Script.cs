e8a00447d2cfbce86ce06ccb5496d91f8699bf47
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2021-05-03 00:19:10 +0900

    [Add]ScreenToWorldPointのスクリプト作成
    しかし、正常には動作しない

--- Assets/osamoya/Scripts/Old_ScreenToWolrd_Script.cs
+++ Assets/osamoya/Scripts/Old_ScreenToWolrd_Script.cs
@@ -13,6 +13,24 @@ public class Old_ScreenToWolrd_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("クリック確認");
+            OnClickScreen();
+        }
+    }
+
+    void OnClickScreen()
+    {
+        Debug.Log("関数確認");
+
+        // マウスのポインタがあるスクリーン座標を取得
+        Vector3 screen_point = Input.mousePosition;
+        // z に 1 を入れないと正しく変換できない
+        screen_point.z = 1.0f;
+        // スクリーン座標をワールド座標に変換
+        Vector3 world_position = Camera.main.ScreenToWorldPoint(screen_point);
+
+        Debug.Log("座標：" + world_position);
     }
 }
