70fae46f818bca3f5251365148d22767d193cb09
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2021-05-03 00:04:22 +0900

    [Add]レイキャストスクリプトのガワを作成
    中身は動かない

---
+++ Assets/osamoya/Scripts/ScreenToWorld_Script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ScreenToWorld_Script : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log(gameObject.name + " がクリックされた!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("HelloWorld!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
