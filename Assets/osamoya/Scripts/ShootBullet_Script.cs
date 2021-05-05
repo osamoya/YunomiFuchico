b3e5aa7f02149e30e44cdd7888a01acc10e72a9d
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2021-05-06 03:33:13 +0900

    [update]発射する関数の作成
    こっちでは呼ばない感じで

--- Assets/osamoya/Scripts/ShootBullet_Script.cs
+++ Assets/osamoya/Scripts/ShootBullet_Script.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ShootBullet_Script : MonoBehaviour
 {
+    [SerializeField] GameObject bullet;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,12 @@ public class ShootBullet_Script : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void shot(Vector3 start,Vector3 angle,float force)
+    {
+        GameObject b=Instantiate(bullet);
+        b.transform.position = start;
+        b.GetComponent<Rigidbody>().AddForce(angle*force);
     }
 }
