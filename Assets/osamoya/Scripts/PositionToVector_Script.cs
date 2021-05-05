15c3b0949eaa6fbec2a31c714ad07d9596a451a7
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2021-05-06 02:56:43 +0900

    [update]変数の宣言と初期化

--- Assets/osamoya/Scripts/PositionToVector_Script.cs
+++ Assets/osamoya/Scripts/PositionToVector_Script.cs
@@ -1,13 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// ・弾は放物線を描いて飛ぶ
+/// ・放物線の２点は指定できるようにする
+/// ・放物線の形を変えることができる
+/// 　　→要素は：速さ・角度と時間
+/// </summary>
 public class PositionToVector_Script : MonoBehaviour
 {
+    [SerializeField] float gravity = 9.8f;
+    [SerializeField] GameObject StartObj;
+    [SerializeField] GameObject TargetObj;
+    Vector3 StartPoint;
+    Vector3 TargetPoint;
+
+    [SerializeField] float reachTime;
+    [SerializeField] float arg;//上向きの角度(xz_y)
+    [SerializeField] float force;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (StartObj != null) StartPoint = StartObj.transform.position;
+        if (TargetObj != null) TargetPoint = TargetObj.transform.position;
     }
 
     // Update is called once per frame
@@ -15,4 +31,7 @@ public class PositionToVector_Script : MonoBehaviour
     {
         
     }
+    
+    
+    
 }
