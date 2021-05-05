145d32ddd477882be30366f945c8026692570d5a
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2021-05-06 01:43:32 +0900

    [update]マウスの位置を検出する
    ただの確認

--- Assets/osamoya/Scripts/CameraRay_Script.cs
+++ Assets/osamoya/Scripts/CameraRay_Script.cs
@@ -7,12 +7,16 @@ public class CameraRay_Script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Camera!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) ShotRay(Input.mousePosition);
+    }
+    void ShotRay(Vector2 mouse)
+    {
+        Debug.Log("mouse："+mouse);
     }
 }
