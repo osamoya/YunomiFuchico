c4f9b65ebe603b3e26c7faef7cbae727d7b1dcda
osamoya <osamumoyamoyasii@yahoo.co.jp>
2021-05-06 13:42:00 +0900

    [update]逆向きに茶柱が回転する

--- Assets/osamoya/Scripts/Pillar_Script.cs
+++ Assets/osamoya/Scripts/Pillar_Script.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Pillar_Script : MonoBehaviour
 {
     //[SerializeField]GameObject pillar;
-    [SerializeField] float speed;
+    [SerializeField] float Yspeed;
+    [SerializeField] float Xspeed;
+
+    float coolTime=0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +18,31 @@ public class Pillar_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localEulerAngles += new Vector3(0, speed, 0);
+        transform.localEulerAngles += new Vector3(0, Yspeed, 0);
 
-        if(Input.GetKeyDown(KeyCode.Z)) transform.Rotate(new Vector3(speed, 0, 0));
-
-
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            PillarRote(1);
+            coolTime = 0;
+        }
+        else
+        {
+            coolTime += Time.deltaTime;
+        }
+        if (coolTime > 3) PillarRote(-0.5f);
     }
 
+    public void PillarRote(float i)
+    {
+        
+        if (i>0&&transform.eulerAngles.x>0)
+        {
+            transform.Rotate(new Vector3(Xspeed * i, 0, 0));
+        }
+
+        if (i<0&&transform.eulerAngles.x<90)
+        {
+            transform.Rotate(new Vector3(Xspeed * i, 0, 0));
+        }
+    }
 }
