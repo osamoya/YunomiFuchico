81a2e04b504d476e329c02c137ed9f6ba1daf910
osamoya <osamumoyamoyasii@yahoo.co.jp>
2021-05-06 13:23:52 +0900

    [update]茶柱の簡単な回転を実装
    transform.localEulerAngles += new Vector3(0, speed, 0);
    if(Input.GetKeyDown(KeyCode.Z)) transform.Rotate(new Vector3(speed, 0, 0));

--- Assets/osamoya/Scripts/Pillar_Script.cs
+++ Assets/osamoya/Scripts/Pillar_Script.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Pillar_Script : MonoBehaviour
 {
+    //[SerializeField]GameObject pillar;
+    [SerializeField] float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,11 @@ public class Pillar_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localEulerAngles += new Vector3(0, speed, 0);
+
+        if(Input.GetKeyDown(KeyCode.Z)) transform.Rotate(new Vector3(speed, 0, 0));
+
+
     }
+
 }
